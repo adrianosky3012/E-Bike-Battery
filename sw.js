5f5d57b0273c3718417a7d40df2b19ac09ed8aa6
adrianosky3012 <adry3012@libero.it>
2026-03-31 13:22:23 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-var CACHE = 'ebike-battery-v1';
-var FILES = ['index.html', 'manifest.json', 'icon-192.png', 'icon-512.png'];
-self.addEventListener('install', function(e){
-  e.waitUntil(caches.open(CACHE).then(function(c){ return c.addAll(FILES); }));
-});
-self.addEventListener('fetch', function(e){
-  e.respondWith(caches.match(e.request).then(function(r){ return r || fetch(e.request); }));
-});
